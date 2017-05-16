fa7cfe64aad0d89cb9bce999e42aeb6125052f31
svolkov <svolkov25@gmail.com>
2017-05-16 15:16:01 +0300

    Add query execution function (draft)

--- assets/dist/app.js
+++ assets/dist/app.js
@@ -377,3 +377,37 @@ $('.query-history-list').on('keypress', '.newQueryName', function(e) {
     }
 });
 /** << Query History **/
+
+/** >> Query execution **/
+var queryExecutionForm = $('#formExecuteQuery');
+
+function initQueryExecutionForm() {
+    var queryParameters = JSON.parse(localStorage.getItem('spe.queryExecution'));
+    if (queryParameters) {
+        for (var key in queryParameters) {
+            var input = queryExecutionForm.find('input[name="' + key + '"]');
+            if (input.length) {
+                input.val(queryParameters[key]);
+            }
+        }
+    }
+}
+
+initQueryExecutionForm();
+
+queryExecutionForm.submit(function(e) {
+    e.preventDefault();
+
+    // Send request
+    var parameters = {
+        'default-graph-uri': $(this).find('input[name="default-graph-uri"]').val(),
+        'query': editor.getValue()
+    };
+    // Save request parameters
+    localStorage.setItem('spe.queryExecution', JSON.stringify(parameters));
+    // Send request
+    //TODO: delete hardcode
+    var requestUrl = 'http://draft.adposium.ru:8890/sparql?' + jQuery.param(parameters);
+    window.open(encodeURI(requestUrl), '_blank');
+});
+/** << Query execution **/
