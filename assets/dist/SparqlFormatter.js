103b959333df8c7f553864298003c7abca454a84
svolkov <svolkov25@gmail.com>
2017-05-18 11:55:25 +0300

    Fix matching triples with shortcut prefixes

--- assets/dist/SparqlFormatter.js
+++ assets/dist/SparqlFormatter.js
@@ -1,6 +1,6 @@
-var triplePairsRegexpCode = '(?:(([?<$][\\w:\\/\\.\\-#>]+)[\\s\\.]+){3}){2}';
-var triplePairLinesRegexpCode = '(([?<$][\\w:\\/\\.\\-#>]+)[\\s\\.]+){3}';
-var triplePairElementsRegexpCode = '([?<$][\\w:\\/\\.\\-#>]+(?!\\w))';
+var triplePairsRegexpCode = '(?:(([?<$\\w][\\w:\\/\\.\\-#>]+)[\\s\\.]+){3}){2}';
+var triplePairLinesRegexpCode = '(([?<$\\w][\\w:\\/\\.\\-#>]+)[\\s\\.]+){3}';
+var triplePairElementsRegexpCode = '([?<$\\w:][\\w:\\/\\.\\-#>]+(?!\\w))';
 var allUriRegexpCode = '[\\w<]+\\:[\\w#\\/\\.-\ v\>-]+';
 
 function deleteIndents()
